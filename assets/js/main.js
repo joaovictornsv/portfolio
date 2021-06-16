6a7bacab65f3b7d23ace53f41f4e9ed2d28726db
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-06-16 00:10:31 -0300

    use localStorage and upload profile pic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
+getTheme();
+
 const checkbox = document.getElementById('theme-checkbox');
 
 const rootCSS = document.documentElement.style;
 
 checkbox.addEventListener('change', toggleTheme);
+
+function getTheme() {
+  const theme = localStorage.getItem('theme');
+  setTimeout(() => {
+    theme === 'light' ? setLightTheme() : setDarkTheme();
+  }, 0)
+
+}
 
 function toggleTheme() {
   checkbox.checked ? setDarkTheme() : setLightTheme();
@@ -12,10 +22,14 @@ function setLightTheme() {
   rootCSS.setProperty('--primary', '#F0F5F9');
   rootCSS.setProperty('--secondary', '#212121');
   rootCSS.setProperty('--secondaryLight', '#393E46');
+  localStorage.setItem('theme', 'light');
+  checkbox.checked = false;
 }
 
 function setDarkTheme() {
   rootCSS.setProperty('--primary','#212121' );
   rootCSS.setProperty('--secondary', '#ececec');
   rootCSS.setProperty('--secondaryLight', '#e5e7e9');
+  localStorage.setItem('theme', 'dark');
+  checkbox.checked = true;
 }
