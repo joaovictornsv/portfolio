caa5773fe3dd14731aa764dbe1850b567fff1daf
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-01-21 11:02:26 -0300

    change tech icon css

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,7 @@ function toggleTheme() {
 
 function setLightTheme() {
   rootCSS.setProperty('--primary', '#F0F5F9');
+  rootCSS.setProperty('--tech-icon-bg', '#e4e8eb');
   rootCSS.setProperty('--secondary', '#212121');
   rootCSS.setProperty('--secondaryLight', '#393E46');
   localStorage.setItem('theme', 'light');
@@ -27,7 +28,8 @@ function setLightTheme() {
 }
 
 function setDarkTheme() {
-  rootCSS.setProperty('--primary','#212121' );
+  rootCSS.setProperty('--primary', '#212121');
+  rootCSS.setProperty('--tech-icon-bg', '#3f3f3f');
   rootCSS.setProperty('--secondary', '#ececec');
   rootCSS.setProperty('--secondaryLight', '#e5e7e9');
   localStorage.setItem('theme', 'dark');
